71ee081c00f3dca5453d192b2defa004529c4866
Caitlin T <catlin.tapper@education.nsw.gov.au>
2025-12-15 20:37:57 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'hehesecret'
+
+def get_db():
+    db = sqlite3.connect('database/assignment.db', timeout = 10)
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.route('/')
+def home():
+    db = get_db()
+    
+    return render_template('home.html')
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        try:
+            db.execute(
+                'INSERT INTO users (username, password, admin) VALUES (?, ?, ?)',
+                (username, generate_password_hash(password), 0)
+            )
+            db.commit()
+            flash('Registration successful! Please log in', 'success')
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash('Username already exists', 'error')
+
+    return render_template('register.html')
+
+app.run(debug=True, port=5000)
